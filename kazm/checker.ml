122d165c132b74503cf78d6d45407ccd3bf20f10
Aapeli <git@aapelivuorinen.com>
2021-11-12 16:15:33 -0500

    Remove user defined funcs

--- kazm/checker.ml
+++ kazm/checker.ml
@@ -2,11 +2,11 @@ open Ast
 
 let check_call call =
   let Call(name, arg) = call in
-    if name = "println" then () else raise (Failure ("Unknown function " ^ name))
+    if String.equal name "println" then () else raise (Failure ("Unknown function " ^ name))
 
 let check_func func =
   let Func(name, calls) = func in
-    List.map check_call calls
+    if String.equal name "main" then List.map check_call calls else raise (Failure ("You can only define main, got " ^ name))
 
 let check prog =
   (* Since the syntax is so simple, just check the called function is valid *)
